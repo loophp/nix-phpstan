1f5f3cae3bea49e799f1a8940442712da97edd31
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-10 19:33:50 +0200

    flake: switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,21 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     phpstan-src = {
       url = "github:phpstan/phpstan";
       flake = false;
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, phpstan-src }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
+  outputs = inputs @ {
+    flake-parts,
+    phpstan-src,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        lib,
+        ...
+      }: let
         php = pkgs.php.buildEnv {
           extraConfig = ''
             memory_limit=-1
@@ -31,37 +43,56 @@
 
           dontUnpack = true;
 
-          nativeBuildInputs = [ pkgs.makeWrapper php ];
+          nativeBuildInputs = [pkgs.makeBinaryWrapper php];
 
           installPhase = ''
             runHook preInstall
+
             mkdir -p $out/bin
             makeWrapper ${php}/bin/php $out/bin/phpstan --add-flags "$src/phpstan.phar"
+
             runHook postInstall
           '';
+
+          meta.mainProgram = "phpstan";
         };
       in {
-        apps = flake-utils.lib.flattenTree {
+        formatter = pkgs.alejandra;
+
+        apps = {
           default = {
             type = "app";
-            program = "${self.packages.${system}.default}";
+            program = phpstan;
           };
         };
-
-        packages = flake-utils.lib.flattenTree {
-          default = phpstan;
-        };
-
-        devShells = {
-          default = pkgs.mkShellNoCC {
-            name = "PHPStan";
-
-            buildInputs = [
-              self.packages.${system}.default
-            ];
-          };
-        };
-      }
-    );
-
+      };
+    };
 }
+#   outputs = { self, flake-utils, nixpkgs, phpstan-src }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs = import nixpkgs {
+#           inherit system;
+#         };
+#       in {
+#         apps = flake-utils.lib.flattenTree {
+#           default = {
+#             type = "app";
+#             program = "${self.packages.${system}.default}";
+#           };
+#         };
+#         packages = flake-utils.lib.flattenTree {
+#           default = phpstan;
+#         };
+#         devShells = {
+#           default = pkgs.mkShellNoCC {
+#             name = "PHPStan";
+#             buildInputs = [
+#               self.packages.${system}.default
+#             ];
+#           };
+#         };
+#       }
+#     );
+# }
+
